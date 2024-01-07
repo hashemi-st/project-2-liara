28cf69ca317e1680380d16d3fa148a44cfb1a8bf
hashemi <hashemi.st@gmail.com>
2024-01-07 19:54:08 +0330

    create web crawler for enamad

---
+++ index.js
@@ -0,0 +1,81 @@
+import fetch from "node-fetch";
+import cheerio from "cheerio";
+import mongoose from "mongoose";
+import Enamad from "./model/enamad.js";
+
+mongoose
+  .connect(
+    "mongodb://root:E3BvZsLQl3dauNVqOohDm0x5@k2.liara.cloud:34395/project-2?authSource=admin"
+  )
+  .then(() => {
+    console.log("Connected!");
+  })
+  .catch((error) => {
+    console.error("Failed to connect!", error);
+  });
+
+const crawl = async ({ url }) => {
+  console.log("crawling", url);
+
+  const response = await fetch(url);
+  const html = await response.text();
+
+  const $ = cheerio.load(html);
+
+  const table = $("div#Div_Content");
+
+  table.find("div.row").each((index, el) => {
+    const row = $(el);
+    const domain = row.find("a").html();
+    const name = row.find("div.col-sm-12.col-md-3").html();
+    const location = row
+      .find("div.col-sm-12.col-md-3+div.col-sm-12.col-md-1")
+      .html();
+
+    const expired = row
+      .find(
+        "div.col-sm-12.col-md-2+div.col-sm-12.col-md-1+div.col-sm-12.col-md-1"
+      )
+      .html();
+
+    const imgs = row
+      .find(
+        "div.col-sm-12.col-md-3+div.col-sm-12.col-md-1+div.col-sm-12.col-md-1+div.col-sm-12.col-md-2"
+      )
+      .html();
+
+    const regex = /\s+<(img).+/gm;
+    const str = imgs;
+    let m;
+    let s = 0;
+    while ((m = regex.exec(str)) !== null) {
+      // This is necessary to avoid infinite loops with zero-width matches
+      if (m.index === regex.lastIndex) {
+        regex.lastIndex++;
+      }
+
+      m.forEach((match, groupIndex) => {
+        s = s + 1;
+      });
+    }
+    const data = new Enamad({
+      domain,
+      name,
+      location,
+      expired,
+      star: s / 2,
+    });
+    data.save();
+  });
+};
+
+let urls = [];
+for (let i = 1; i <= 100; i++) {
+  urls.push(`https://enamad.ir/DomainListForMIMT/Index/${i}`);
+}
+
+urls.map((url) =>
+  crawl({
+    url,
+  })
+);
